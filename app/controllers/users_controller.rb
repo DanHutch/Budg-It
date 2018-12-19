720e63f9c79b6c404533c50d1a39feca3079410b
Dan Hutchinson <danhutch5280@gmail.com>
2018-12-19 15:45:07 -0700

    4 in progress commit, building out OAuth functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,31 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
-	
+	def create
+		@user = User.new(ready_params)
+		if @user.save
+      session[:user_id] = @user.id
+      flash[:message] = "Logged in as #{@user.name}"
+    else
+      flash[:error] = 'Username already exists'
+      render :new
+    end
+		# redirect_to login_path
+	end
+
+	private
+
+	def ready_params
+		new_params = user_params
+		raw_phone = new_params["phone"]
+		new_phone = raw_phone.scan(/\d+/).join
+		new_phone[0] == "1" ? new_phone[0] = "" : new_phone
+		new_params["phone"] = new_phone unless new_phone.length != 10
+		return new_params
+	end
+
+	def user_params
+   params.require(:user).permit(:email, :name, :phone)
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   get "/", to: "welcome#index", as: "root"
 
   get "/register", to: "users#new", as: "registration"
-  get "/login", to: "session#new", as: "login"
+  
+  # get "/login", to: "session#new", as: "login"
+  get 'login', to: "sessions#new", as: 'login'
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+
 
   get "/privacy", to: "privacy#show", as: "privacy"
 
