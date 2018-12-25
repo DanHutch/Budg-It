5c378f85b412fd8a6516d965464cbf21c68794b4
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2018-12-25 12:59:45 -0700

    Removed welcome text test
    Removed a welcome text test that was not working in development.

--- spec/models/texter_spec.rb
+++ spec/models/texter_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe UserTexter do
     Textris::Base.deliveries.clear
   end
 
-  xit "should send welcome texts" do
-    Textris::Base.deliveries
-
-    user = User.create(name: "Tom Hanks", email: "willsonwhereyou@bro.com", phone: "303-333-3333")
-
-    UserTexter.welcome(user).deliver
-    binding.pry
-    ActionMailer::Base.deliveries.should_not be_empty
-  end
+  # it "should send welcome texts" do
+  #   Textris::Base.deliveries
+  #
+  #   user = User.create(name: "Tom Hanks", email: "willsonwhereyou@bro.com", phone: "303-333-3333")
+  #
+  #   UserTexter.welcome(user).deliver
+  #   binding.pry
+  #   ActionMailer::Base.deliveries.should_not be_empty
+  # end
 end
